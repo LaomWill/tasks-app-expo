0127bbde98814b626c6d8afce88ca648a6202076
Renan Costa Alencar <renan.costaalencar@gmail.com>
2026-04-02 17:58:47 -0300

    feat: center loading indicator with overlay and add react-native-web support

--- App.tsx
+++ App.tsx
@@ -74,7 +74,9 @@ export default function App() {
         />
 
         {loading && (
-          <ActivityIndicator size="large" color="#000" style={styles.loader} />
+          <View style={styles.loaderContainer}>
+            <ActivityIndicator size="large" color="#000" />
+          </View>
         )}
       </View>
       <StatusBar style="auto" />
@@ -150,7 +152,15 @@ const styles = StyleSheet.create({
   nativeButtonContainer: {
     marginTop: 16,
   },
-  loader: {
-    marginTop: 16,
+  loaderContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    zIndex: 10,
   }
 });
