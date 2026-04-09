80738cac3cca85ba415a7606e7a3fd229e4aefcc
Luan Martins <136653630+LaomWill@users.noreply.github.com>
2026-04-09 20:33:32 -0300

    Questão 1

---
+++ styles/global.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  primaryColor: {
+    color: '#000000',
+  },
+  backgroundColor: {
+    backgroundColor: '#FFFFFF',
+  },
+  bodyFontSize: {
+    fontSize: 16,
+  },
+});
